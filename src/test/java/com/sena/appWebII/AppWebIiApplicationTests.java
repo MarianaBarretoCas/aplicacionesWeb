05dfa0dc88ba5b4bc88d50c60424bd45154aaac2
Maris <marianabarretocas@gmail.com>
2025-08-10 10:21:57 -0500

    Primer commit

---
+++ src/test/java/com/sena/appWebII/AppWebIiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.appWebII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
